be5da3cbea8c9e4986600d4afd7dbead24d0524f
Tom Aarsen <Cubiegamedev@gmail.com>
2022-04-29 10:05:17 +0200

    Add initial save_model script

---
+++ scripts/save_model.sh
@@ -0,0 +1,26 @@
+if [ $# -ne 2 ]; then
+    echo "Please supply a path to the best and last model to save."
+    exit 1
+fi
+
+best=$1
+last=$2
+
+# Save the current changes that might exist on main
+git stash
+
+# Move to the models branch
+git checkout models
+
+# Update the models
+clear models/
+cp $best models/
+cp $last models/
+git add models/
+git commit -m "Save new models & clear previous ones"
+
+# Move back to main
+git checkout main
+
+# Restore last changes in the stash
+git stash pop
