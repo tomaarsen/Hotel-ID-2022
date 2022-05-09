3d080da8a4e8948def6cf04610c20d4deb212f5f
Tom Aarsen <Cubiegamedev@gmail.com>
2022-05-09 16:21:19 +0200

    Adding base embedding comparison

--- cli_evaluate.py
+++ cli_evaluate.py
@@ -15,7 +15,11 @@ import numpy as np
 import pandas as pd
 
 import torch as t
+from torch.utils.data import DataLoader
 from tqdm import tqdm
+from skeleton.data.collating import collate_hid
+from skeleton.data.dataset import ImageDataset
+from skeleton.data.hotel_id import HotelIDDataModule
 
 from skeleton.data.preprocess import Preprocessor
 from skeleton.models.prototype import HotelID
@@ -51,21 +55,56 @@ def main(
     # set to eval mode, and move to GPU if applicable
     model = model.eval()
 
+    # Get the list of hotel_ids, i.e. a mapping of index/label to hotel ID
     hotel_ids = sorted(
         int(folder.stem) for folder in (data_folder / "train_images").iterdir()
     )
 
-    test_image_files = list((data_folder / "test_images").iterdir())
-    predictions = []
-    for image_file in tqdm(test_image_files):
-        image = Image.open(image_file).convert("RGB")
-        image = np.asarray(image)
-        image = preprocessor.val_transform(image=image)["image"]
-        image = image.unsqueeze(0)
-        output = model(image)
-        indices = list(output.squeeze().topk(5).indices)
-        hids = " ".join(str(hotel_ids[int(index)]) for index in indices)
-        predictions.append(hids)
+    # hotelid_dm = HotelIDDataModule(data_folder, hotel_ids, 1, 2, preprocessor, 0.0)
+    # base_dl = hotelid_dm.val_dataloader()
+    # base_embeddings = [sample for sample in base_dl]
+    # breakpoint()
+    with t.no_grad():
+        # Generate the base embeddings...
+        test_ds = ImageDataset(
+            list((data_folder / "train_images").glob("**/*.jpg")),
+            hotel_ids,
+            transform=preprocessor.val_transform,
+        )
+
+        test_dl = DataLoader(test_ds, batch_size=16, num_workers=2, collate_fn=collate_hid)
+
+        base_embeddings = t.tensor([])
+        base_hotel_ids = t.tensor([])
+        for batch in tqdm(test_dl, desc="Generating base embeddings"):
+            base_embeddings = t.cat((base_embeddings, model(batch.images)))
+            base_hotel_ids = t.cat((base_hotel_ids, batch.hotel_ids))
+        # Base embeddings generated.
+
+        # List of image paths and the corresponding predictions
+        test_image_files = list((data_folder / "test_images").iterdir())
+        predictions = []
+
+        for image_file in tqdm(test_image_files):
+            prediction = []
+            
+            # Apply validation transformations to the test image
+            image = Image.open(image_file).convert("RGB")
+            image = np.asarray(image)
+            image = preprocessor.val_transform(image=image)["image"]
+            image = image.unsqueeze(0)
+
+            # Get the embedding, and the 5 hotels with the most similar embeddings
+            embedding = model(image)
+            distances = t.cosine_similarity(embedding, base_embeddings)
+            sorted_dist, indices = distances.sort(descending=True)
+            for hid in base_hotel_ids[indices]:
+                if hid in prediction:
+                    continue
+                prediction.append(hid)
+                if len(prediction) == 5:
+                    break
+            predictions.append(" ".join(str(int(pred)) for pred in prediction))
 
     df = pd.DataFrame(
         data={
@@ -74,6 +113,7 @@ def main(
         }
     ).sort_values(by="image_id")
     df.to_csv("submission.csv", index=False)
+    print(df.head())
 
 
 if __name__ == "__main__":
