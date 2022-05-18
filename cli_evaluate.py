9af69f089877314976306e4eca5d3811466c10eb
Tom Aarsen <Cubiegamedev@gmail.com>
2022-05-18 13:31:33 +0200

    Add base embeddings to list of embeddings like intended

--- cli_evaluate.py
+++ cli_evaluate.py
@@ -37,6 +37,7 @@ ENSEMBLE = [
         "val_acc": 0.592,
         "id": 1926876,
         "hash": "0d779c0352980ffb6db9a1cda3f6f412983544b2",
+        "checkpoint": "epoch_0019.step_000201160.val-loss_3.0714.last.ckpt",
     },
     {
         "model": "efficientnet_b3 + ArcFace",
@@ -44,6 +45,7 @@ ENSEMBLE = [
         "val_acc": 0.461,
         "id": 2006869,
         "hash": "d3ac50adff07b7146a6b72a1deb36f1dd1af3e61",
+        "checkpoint": "epoch_0019.step_000100580.val-loss_4.0622.last.ckpt",
     },
     {
         "model": "eca_nfnet_l2 + CosFace",
@@ -51,6 +53,7 @@ ENSEMBLE = [
         "val_acc": None,
         "id": 1997481,
         "hash": "aa86a11f8ebd9099f7350ec9d95e8ed3579b7fd6",
+        "checkpoint": "epoch_0019.step_000201160.val-loss_13.1027.best.ckpt",
     },
 ]
 
@@ -133,6 +136,7 @@ def main(
                 base_embeddings = t.cat((base_embeddings, model(batch.images)))
                 base_hotel_ids = t.cat((base_hotel_ids, batch.hotel_ids))
             # Base embeddings generated.
+            all_base_embeddings.append(base_embeddings)
 
     # List of image paths and the corresponding predictions
     test_image_files = list((data_folder / "test_images").iterdir())
