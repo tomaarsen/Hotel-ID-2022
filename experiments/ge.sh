aa68aa5e1c7d30ccf0df8f603cc080657f7fa8df
Tom Aarsen <Cubiegamedev@gmail.com>
2022-05-21 17:04:32 +0200

    Reduce CPU requirement

--- experiments/ge.sh
+++ experiments/ge.sh
@@ -2,7 +2,7 @@
 #SBATCH --partition=csedu
 #SBATCH --gres=gpu:1
 #SBATCH --mem=16G
-#SBATCH --cpus-per-task=6
+#SBATCH --cpus-per-task=2
 #SBATCH --time=24:00:00
 #SBATCH --output=./logs/slurm/%J.out
 #SBATCH --error=./logs/slurm/%J.err
