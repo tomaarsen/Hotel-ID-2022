53d4e7f6c4bbb1c8374733b41b52606617fd6578
Tom Aarsen <Cubiegamedev@gmail.com>
2022-05-21 16:57:35 +0200

    Add new models to ensemble

--- ensemble.py
+++ ensemble.py
@@ -21,10 +21,31 @@ ENSEMBLE = {
         "hash": "aa86a11f8ebd9099f7350ec9d95e8ed3579b7fd6",
     },
     "epoch_0019.step_000050280.val-loss_4.4766.last.ckpt": {
-        "model": "vit_small_patch16_384 + ArcFace",
+        "model": "vit_small_patch16_384 + ArcFace (20 epochs)",
         "map@5": None,
         "val_acc": 0.422,
         "id": 2016869,
+        "hash": "d3ac50adff07b7146a6b72a1deb36f1dd1af3e61",
+    },
+    "epoch_0049.step_000125700.val-loss_3.2614.best.ckpt": {
+        "model": "vit_small_patch16_384 + ArcFace (50 epochs)",
+        "map@5": None,
+        "val_acc": 0.557,
+        "id": 2031170,
+        "hash": "d3ac50adff07b7146a6b72a1deb36f1dd1af3e61",
+    },
+    "epoch_0059.step_000301740.val-loss_3.7426.best.ckpt": {
+        "model": "regnety_120 + ArcFace (60 epochs)",
+        "map@5": None,
+        "val_acc": 0.535,
+        "id": 2030533,
+        "hash": "d3ac50adff07b7146a6b72a1deb36f1dd1af3e61",
+    },
+    "epoch_0059.step_000301740.val-loss_3.8213.last.ckpt": {
+        "model": "resnest101e + ArcFace (60 epochs)",
+        "map@5": None,
+        "val_acc": 0.531,
+        "id": 2030534,
         "hash": "d3ac50adff07b7146a6b72a1deb36f1dd1af3e61",
     },
 }
